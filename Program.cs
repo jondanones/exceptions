cd4d2baa6e93d424c7e9e7d6724d7ac6e73b9fec
Jow <john.sama64@gmail.com>
2024-09-29 12:28:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using exercExcept.Entities;
+using exercExcept.Entities.Exceptions;
+
+Console.WriteLine("Enter account data");
+Console.Write("Number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Holder: ");
+string holder = Console.ReadLine();
+Console.Write("Initial balance: ");
+double balance = double.Parse(Console.ReadLine());
+Console.Write("Withdraw limit: ");
+double withdrawLimit = double.Parse(Console.ReadLine());
+
+Account acc = new Account(number, holder, balance, withdrawLimit);
+Console.WriteLine();
+Console.Write("Enter amount for withdraw: ");
+double amount = double.Parse(Console.ReadLine());
+try
+{
+    acc.Withdraw(amount);
+    Console.WriteLine("New balance: " + acc.Balance);
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Withdraw error: " + e.Message);
+}
